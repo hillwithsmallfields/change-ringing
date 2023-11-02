82e3368e38f1fecc9772bc14bfbde38e8e637cc7
John Sturdy <jcg.sturdy@gmail.com>
2023-11-02 19:57:21 +0000

    Add program for reading back which towers are done

---
+++ dove_josm/get_done_doves.py
@@ -0,0 +1,25 @@
+#!/usr/bin/env python3
+
+import argparse
+import csv
+import os
+
+import overpy
+
+def get_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--output-file", "-o",
+                        default="~/ringing/doves-done.csv")
+    return vars(parser.parse_args())
+
+def get_done_doves(output_file):
+    api = overpy.Overpass()
+    result = api.query("""[out:json]; way ["ref:dove"]; out tags;""")
+    with open(os.path.expanduser(output_file), "w") as outstream:
+        writer = csv.writer(outstream)
+        writer.writerow(["OSM ID", "Dove ID"])
+        for way in result.ways:
+            writer.writerow([way.id, way.tags["ref:dove"]])
+
+if __name__ == "__main__":
+    get_done_doves(**get_args())
