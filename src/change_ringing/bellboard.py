55a80c6b7bbcd657ff9848cf9ec6ac86d369a426
John Sturdy <jcg.sturdy@gmail.com>
2026-03-08 16:29:04 +0000

    add listing by towers

--- src/change_ringing/bellboard.py
+++ src/change_ringing/bellboard.py
@@ -1,10 +1,13 @@
 #!/usr/bin/env python3
 
 import argparse
+import collections
 import csv
 import os
 import re
 import requests
+
+import towers
 
 """Read, expand and normalise BellBoard data from its CSV output."""
 
@@ -20,7 +23,8 @@ def get_args():
     parser.add_argument("--region", "--county", "-c")
     parser.add_argument("--filename", "--file", "-f")
     parser.add_argument("--since", "-s")
-    parser.add_argument("--bells", "-b", type=int, default='4+')
+    parser.add_argument("--bells", "-b", default='4+')
+    parser.add_argument("--tower-details", "--towers", action='store_true')
     parser.add_argument("--verbose", "-v", action='store_true')
     return vars(parser.parse_args())
 
@@ -125,14 +129,45 @@ def performances(place=None, region=None, since=None, bells='4+', verbose=False)
     if verbose:
         print("Using query:", query)
     response = requests.get("https://bb.ringingworld.co.uk/export.php", query)
-    return parse_performance_list(response.content) if (response.status_code == 200) else None
+    if (response.status_code == 200):
+        result = parse_performance_list(response.content)
+        return result
+    return None
 
-def main(place=None, region=None, filename=None, since=None, bells='4+', verbose=False):
+dove_data = None
+
+def get_dove_data():
+    global dove_data
+    if not dove_data:
+        dove_data = towers.read_dove()
+    return dove_data
+
+def by_tower(perfs):
+    """Combine performance and tower data."""
+    by_tower = collections.defaultdict(list)
+    tower_data = get_dove_data()
+    for performance in perfs:
+        by_tower[performance.place].append(performance)
+    return by_tower
+
+def list_tower_performances(by_towers):
+    tower_data = get_dove_data()
+    for tower_name in sorted(by_towers.keys(), key=lambda place: len(by_towers[place]), reverse=True):
+        tower_details = tower_data.get(tower_name)
+        rung_here = by_towers[tower_name]
+        print("%s: %d performances" % (tower_name, len(rung_here)))
+        for touch in rung_here:
+            print("  ", ", ".join(touch.methods))
+
+def main(place=None, region=None, filename=None, since=None, bells='4+', tower_details=False, verbose=False):
     data = (parse_performance_list(filename)
             if filename
-            else performances(place, region, since, bells, verbose))
-    for p in data:
-        print(p)
+            else performances(place=place, region=region, since=since, bells=bells, verbose=verbose))
+    if tower_details:
+        list_tower_performances(by_tower(data))
+    else:
+        for p in data:
+            print(p)
 
 # https://bb.ringingworld.co.uk/export.php?from=01%2F01%2F2023&region=Cambridgeshire&bells_type=tower&fmt=csv_header
 
