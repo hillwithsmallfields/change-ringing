54e44d44d0a626e3faea4a2a4f2d777148e93582
John Sturdy <jcg.sturdy@gmail.com>
2026-03-08 15:20:52 +0000

    more packaging stuff

--- setup.py
+++ setup.py
@@ -1,7 +1,7 @@
 from setuptools import setup, find_packages
 
 setup(
-    name="qs",
+    name="change_ringing",
     version="0.0.1",
     description="My assorted change-ringing code.",
     author="John C. G. Sturdy",
