bbe33c89e84ae8fdffbf4f1e900ae53d9b725a98
John Sturdy <jcg.sturdy@gmail.com>
2026-03-08 14:33:35 +0000

    add packaging files

---
+++ setup.py
@@ -0,0 +1,10 @@
+from setuptools import setup, find_packages
+
+setup(
+    name="qs",
+    version="0.0.1",
+    description="My assorted change-ringing code.",
+    author="John C. G. Sturdy",
+    author_email="jcg.sturdy@gmail.com",
+    packages=find_packages(),
+)
