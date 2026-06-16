d12f67e141c949a7efcf28a73cd87e5a7aeca583
John Sturdy <jcg.sturdy@gmail.com>
2026-06-16 14:36:23 +0100

    fixes to package setup

--- setup.py
+++ setup.py
@@ -2,9 +2,8 @@ from setuptools import setup, find_packages
 
 setup(
     name="change_ringing",
-    version="0.0.1",
+    version="0.0.2",
     description="My assorted change-ringing code.",
     author="John C. G. Sturdy",
     author_email="jcg.sturdy@gmail.com",
-    packages=find_packages(),
 )
